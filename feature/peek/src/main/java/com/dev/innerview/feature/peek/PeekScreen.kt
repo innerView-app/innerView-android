ab23dc82750dff703024f6e10a168ea43a7121a4
p-chanmin <pc_m111@naver.com>
2024-08-12 22:04:30 +0900

    IV-130 feat: peek screen 기본 설정

--- feature/peek/src/main/java/com/dev/innerview/feature/peek/PeekScreen.kt
+++ feature/peek/src/main/java/com/dev/innerview/feature/peek/PeekScreen.kt
@@ -1,17 +1,30 @@
 package com.dev.innerview.feature.peek
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.dev.innerview.core.designsystem.component.InnerViewAppBarIcon
+import com.dev.innerview.core.designsystem.component.InnerViewTopAppBar
+import com.dev.innerview.core.designsystem.component.TopAppBarNavigationType
+import com.dev.innerview.core.designsystem.theme.InnerViewTheme
 import kotlinx.coroutines.flow.collectLatest
 
 @Composable
@@ -33,15 +46,24 @@ internal fun PeekRoute(
 private fun PeekScreen(
     padding: PaddingValues,
 ) {
-    val scrollState = rememberScrollState()
-    Column(
-        Modifier
+    Box(
+        modifier = Modifier
             .padding(padding)
-            .padding(horizontal = 8.dp)
-            .verticalScroll(scrollState)
-            .padding(bottom = 4.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
     ) {
-        Text("Peek Screen")
+        Text(
+            text = "Peek Screen",
+            style = MaterialTheme.typography.titleMedium
+        )
+    }
+}
+
+@Composable
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+private fun PeekScreenPreview() {
+    InnerViewTheme {
+        PeekScreen(padding = PaddingValues())
     }
 }
