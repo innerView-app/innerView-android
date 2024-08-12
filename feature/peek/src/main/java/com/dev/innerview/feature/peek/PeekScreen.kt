abe15dc2a0b562fb27f31d0da5a70c0a5e813afc
p-chanmin <pc_m111@naver.com>
2024-08-13 01:15:41 +0900

    IV-130 chore: peek screen import 정리

--- feature/peek/src/main/java/com/dev/innerview/feature/peek/PeekScreen.kt
+++ feature/peek/src/main/java/com/dev/innerview/feature/peek/PeekScreen.kt
@@ -1,29 +1,18 @@
 package com.dev.innerview.feature.peek
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.dev.innerview.core.designsystem.component.InnerViewAppBarIcon
-import com.dev.innerview.core.designsystem.component.InnerViewTopAppBar
-import com.dev.innerview.core.designsystem.component.TopAppBarNavigationType
 import com.dev.innerview.core.designsystem.theme.InnerViewTheme
 import kotlinx.coroutines.flow.collectLatest
 
