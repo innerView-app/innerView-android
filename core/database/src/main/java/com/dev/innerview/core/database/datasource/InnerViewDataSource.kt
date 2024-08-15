a318eaae69c1473d5401b2f6f8828c05831fda01
p-chanmin <pc_m111@naver.com>
2024-08-16 03:31:53 +0900

    IV-131 feat: InnerViewDataSource 생성

---
+++ core/database/src/main/java/com/dev/innerview/core/database/datasource/InnerViewDataSource.kt
@@ -0,0 +1,57 @@
+package com.dev.innerview.core.database.datasource
+
+import com.dev.innerview.core.database.schema.InnerViewSchema
+import com.dev.innerview.core.database.schema.ProjectSchema
+import io.realm.kotlin.Realm
+import io.realm.kotlin.ext.query
+import io.realm.kotlin.query.find
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class InnerViewDataSource @Inject constructor(
+    private val realm: Realm
+) {
+
+    val innerViewData = realm
+        .query<InnerViewSchema>()
+        .asFlow()
+        .map { result ->
+            result.list.toList()
+        }
+
+    val projectData = realm
+        .query<ProjectSchema>()
+        .asFlow()
+        .map { result ->
+            result.list.toList()
+        }
+
+    suspend fun addInnerView(
+        title: String,
+        type: String,
+        createdAt: String,
+    ) {
+        val id = getNextPrimaryKey()
+
+        val newInnerView = InnerViewSchema().apply {
+            this._id = id
+            this.title = title
+            this.type = type
+            this.createdAt = createdAt
+        }
+
+        realm.write {
+            copyToRealm(newInnerView)
+        }
+    }
+
+    private fun getNextPrimaryKey(): Int {
+        return realm.query<InnerViewSchema>().max("_id", Int::class).find { i ->
+            if (i == null) {
+                1
+            } else {
+                i + 1
+            }
+        }
+    }
+}
