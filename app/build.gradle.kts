2c1759c7039dcf6726cf18335dedbaf5d54a53d9
LeeHaiLim <hailim1997@gmail.com>
2024-08-16 20:20:56 +0900

    CI/CD - 빌드 설정 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
 plugins {
     id("innerview.android.application")
 }
@@ -22,6 +24,26 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
+    signingConfigs {
+        create("releaseConfig") {
+            keyAlias = getPropertyKey("SIGNED_KEY_ALIAS")
+            keyPassword = getPropertyKey("SIGNED_KEY_PASSWORD")
+            storePassword = getPropertyKey("SIGNED_STORE_PASSWORD")
+            storeFile = file(getPropertyKey("SIGNED_STORE_FILE"))
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("releaseConfig")
+            isDebuggable = false
+        }
+    }
+}
+
+fun getPropertyKey(propertyKey: String): String {
+    return gradleLocalProperties(rootDir, providers).getProperty(propertyKey)
 }
 
 dependencies {
