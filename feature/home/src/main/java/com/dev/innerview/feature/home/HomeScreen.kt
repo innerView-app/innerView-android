966d8ad33417cdde4bfd90ac0121fff34942f6a9
p-chanmin <pc_m111@naver.com>
2024-08-17 03:49:28 +0900

    IV-18 feat: HomeViewModel 연결

--- feature/home/src/main/java/com/dev/innerview/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/com/dev/innerview/feature/home/HomeScreen.kt
@@ -1,22 +1,35 @@
 package com.dev.innerview.feature.home
 
 import android.content.res.Configuration
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.dev.innerview.core.designsystem.component.InnerViewAppBarIcon
+import com.dev.innerview.core.designsystem.component.InnerViewFloatingActionButton
 import com.dev.innerview.core.designsystem.component.InnerViewTopAppBar
 import com.dev.innerview.core.designsystem.component.TopAppBarNavigationType
+import com.dev.innerview.core.designsystem.component.appBarSize
 import com.dev.innerview.core.designsystem.theme.InnerViewTheme
+import com.dev.innerview.core.designsystem.theme.Paddings
+import com.dev.innerview.feature.home.model.HomeUiState
 import kotlinx.coroutines.flow.collectLatest
 
 @Composable
@@ -26,18 +39,24 @@ internal fun HomeRoute(
     onInnerViewClick: (String) -> Unit,
     viewModel: HomeViewModel = hiltViewModel(),
 ) {
+
+    val homeUiState by viewModel.homeUiState.collectAsStateWithLifecycle()
+
     LaunchedEffect(true) {
         viewModel.errorFlow.collectLatest { throwable -> onShowErrorSnackBar(throwable) }
     }
 
     HomeScreen(
+        homeUiState = homeUiState,
         padding = padding,
         onInnerViewClick = onInnerViewClick
     )
 }
 
+
 @Composable
 private fun HomeScreen(
+    homeUiState: HomeUiState,
     padding: PaddingValues,
     onInnerViewClick: (String) -> Unit
 ) {
@@ -47,7 +66,7 @@ private fun HomeScreen(
             .fillMaxSize()
     ) {
         InnerViewTopAppBar(
-            titleString = stringResource(R.string.feature_home_innerview_screen_title),
+            title = stringResource(id = R.string.feature_home_innerview_screen_title),
             navigationType = TopAppBarNavigationType.None,
             actionButtons = {
                 InnerViewAppBarIcon(
@@ -56,6 +75,39 @@ private fun HomeScreen(
                 )
             }
         )
+
+        Box(
+            modifier = Modifier
+                .padding(top = appBarSize)
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+        ) {
+            when (homeUiState) {
+                is HomeUiState.Loading -> Loading()
+                is HomeUiState.UiState -> {
+                    Column(
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+
+                    }
+                    InnerViewFloatingActionButton(
+                        modifier = Modifier
+                            .align(Alignment.BottomEnd)
+                            .padding(end = Paddings.large, bottom = Paddings.large),
+                        iconImageVector = Icons.Filled.Add,
+                        text = stringResource(R.string.feature_home_innerview_create),
+                        onClick = {}
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun Loading() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        CircularProgressIndicator()
     }
 }
 
@@ -64,6 +116,10 @@ private fun HomeScreen(
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 private fun HomeScreenPreview() {
     InnerViewTheme {
-        HomeScreen(padding = PaddingValues(), onInnerViewClick = {})
+        HomeScreen(
+            homeUiState = HomeUiState.UiState(),
+            padding = PaddingValues(),
+            onInnerViewClick = {}
+        )
     }
 }
--- feature/home/src/main/java/com/dev/innerview/feature/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/dev/innerview/feature/home/HomeViewModel.kt
@@ -1,15 +1,37 @@
 package com.dev.innerview.feature.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dev.innerview.core.domain.usecase.GetInnerViewUseCase
+import com.dev.innerview.feature.home.model.HomeUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.collections.immutable.toPersistentList
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor() : ViewModel() {
+class HomeViewModel @Inject constructor(
+    getInnerViewUseCase: GetInnerViewUseCase
+) : ViewModel() {
 
     private val _errorFlow = MutableSharedFlow<Throwable>()
     val errorFlow get() = _errorFlow.asSharedFlow()
 
+    private val _homeUiState =
+        MutableStateFlow<HomeUiState>(HomeUiState.Loading)
+    val homeUiState = _homeUiState.asStateFlow()
+
+    init {
+        getInnerViewUseCase()
+            .onEach { innerViews ->
+                _homeUiState.value = HomeUiState.UiState(
+                    innerViews = innerViews.toPersistentList()
+                )
+            }.launchIn(viewModelScope)
+    }
 }
