7d0928a37066b3d14e0dcd7c8ea72b96a3687c46
p-chanmin <pc_m111@naver.com>
2024-08-16 18:50:45 +0900

    IV-18 feat: core.model module 생성

--- build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -25,6 +25,7 @@ class AndroidFeatureConventionPlugin : Plugin<Project> {
                 add("implementation", libs.findLibrary("androidx.compose.navigation").get())
                 add("androidTestImplementation", libs.findLibrary("androidx.compose.navigation.test").get())
 
+                add("implementation", project(":core:model"))
                 add("implementation", project(":core:designsystem"))
                 add("implementation", project(":core:navigation"))
                 add("implementation", project(":core:database"))
