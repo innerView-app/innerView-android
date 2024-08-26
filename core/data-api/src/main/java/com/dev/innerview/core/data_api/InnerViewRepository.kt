017c98e7728c0cb417b427adcd340a46f9ecd8dc
p-chanmin <pc_m111@naver.com>
2024-08-26 12:46:12 +0900

    IV-31 feat: InnerViewRepository api 추가

--- core/data-api/src/main/java/com/dev/innerview/core/data_api/InnerViewRepository.kt
+++ core/data-api/src/main/java/com/dev/innerview/core/data_api/InnerViewRepository.kt
@@ -2,13 +2,55 @@ package com.dev.innerview.core.data_api
 
 import com.dev.innerview.core.model.InnerView
 import com.dev.innerview.core.model.InnerViewType
+import com.dev.innerview.core.model.Interview
+import com.dev.innerview.core.model.InterviewGroup
 import kotlinx.coroutines.flow.Flow
 
 interface InnerViewRepository {
 
     fun getInnerViews(): Flow<List<InnerView>>
 
+    fun getInterviewGroups(innerViewId: String): Flow<List<InterviewGroup>>
+
+    fun getInterviews(innerViewId: String, interviewGroupId: String): Flow<List<Interview>>
+
     suspend fun addInnerView(title: String, type: InnerViewType)
 
     suspend fun deleteInnerView(id: String)
+
+    suspend fun addInterviewGroup(innerViewId: String)
+
+    suspend fun deleteInterviewGroup(
+        innerViewId: String,
+        interviewGroupId: String,
+    )
+
+    suspend fun addQuestion(
+        innerViewId: String,
+        interviewGroupId: String,
+        question: String
+    )
+
+    suspend fun deleteQuestion(
+        innerViewId: String,
+        interviewGroupId: String,
+        question: String
+    )
+
+    suspend fun addInnerProject(
+        innerViewId: String,
+        interviewGroupId: String,
+        question: String,
+    )
+
+    suspend fun deleteInnerProject(
+        innerViewId: String,
+        interviewGroupId: String,
+        question: String,
+    )
+
+    suspend fun completeInterviewGroup(
+        innerViewId: String,
+        interviewGroupId: String
+    )
 }
