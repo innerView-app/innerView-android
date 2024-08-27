dc6e9753bd8f7d17be72b5401d701bdebe748abe
p-chanmin <pc_m111@naver.com>
2024-08-27 22:55:20 +0900

    IV-31 chore: core model 기본값 추가

--- core/model/src/main/java/com/dev/innerview/core/model/Interview.kt
+++ core/model/src/main/java/com/dev/innerview/core/model/Interview.kt
@@ -3,9 +3,9 @@ package com.dev.innerview.core.model
 import java.time.ZonedDateTime
 
 data class Interview(
-    val createdAt: ZonedDateTime?,
-    val question: String,
-    var isRequired: Boolean,
-    val isRecordComplete: Boolean,
-    val thumbnailVideoPath: String?
+    val createdAt: ZonedDateTime? = null,
+    val question: String = "",
+    var isRequired: Boolean = false,
+    val isRecordComplete: Boolean = false,
+    val thumbnailVideoPath: String? = null
 )
--- core/model/src/main/java/com/dev/innerview/core/model/InterviewGroup.kt
+++ core/model/src/main/java/com/dev/innerview/core/model/InterviewGroup.kt
@@ -3,9 +3,9 @@ package com.dev.innerview.core.model
 import java.time.ZonedDateTime
 
 data class InterviewGroup(
-    val id: Int,
-    val createdAt: ZonedDateTime,
-    val recordState: RecordState,
-    val questionCount: Int,
-    val thumbnailVideoPath: String?
+    val id: Int = 0,
+    val createdAt: ZonedDateTime = ZonedDateTime.now(),
+    val recordState: RecordState = RecordState.RECODING,
+    val questionCount: Int = 0,
+    val thumbnailVideoPath: String? = null
 )
