eeb4496c8a6f4b74f5300f3250aa2ecc43347275
LeeHaiLim <hailim1997@gmail.com>
2024-08-28 21:12:10 +0900

    IV-50 feat: innerview detail 뷰모델 data fetch

--- feature/home/src/main/java/com/dev/innerview/feature/home/InnerViewDetailViewModel.kt
+++ feature/home/src/main/java/com/dev/innerview/feature/home/InnerViewDetailViewModel.kt
@@ -3,7 +3,9 @@ package com.dev.innerview.feature.home
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.dev.innerview.core.domain.usecase.AddInterviewGroupUseCase
-import com.dev.innerview.core.domain.usecase.GetInterviewGroupUseCase
+import com.dev.innerview.core.domain.usecase.GetInnerViewContentUseCase
+import com.dev.innerview.core.model.InnerViewType
+import com.dev.innerview.core.model.InterviewGroup
 import com.dev.innerview.feature.home.model.InnerViewDetailUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.collections.immutable.toPersistentList
@@ -15,11 +17,13 @@ import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import java.time.LocalDate
+import java.time.ZoneId
 import javax.inject.Inject
 
 @HiltViewModel
 class InnerViewDetailViewModel @Inject constructor(
-    private val getInterviewGroupUseCase: GetInterviewGroupUseCase,
+    private val getInnerViewContentUseCase: GetInnerViewContentUseCase,
     private val addInterviewGroupUseCase: AddInterviewGroupUseCase
 ) : ViewModel() {
 
@@ -30,12 +34,40 @@ class InnerViewDetailViewModel @Inject constructor(
     val innerViewDetailUiState = _innerViewDetailUiState.asStateFlow()
 
     fun fetchInnerView(innerViewId: String) {
-        getInterviewGroupUseCase(innerViewId)
-            .onEach { interviewGroups ->
+        getInnerViewContentUseCase(innerViewId)
+            .onEach { innerViewContent ->
+                val reactivateAt = findReactivateDate(
+                    innerViewContent.interviewGroups,
+                    innerViewContent.innerView?.type ?: InnerViewType.YEAR
+                )
                 _innerViewDetailUiState.update {
-                    it.copy(interviewGroups = interviewGroups.toPersistentList())
+                    it.copy(
+                        title = innerViewContent.innerView?.title ?: "",
+                        interviewGroups = innerViewContent.interviewGroups.toPersistentList(),
+                        reactivateAt = reactivateAt,
+                        isActivated = !LocalDate.now().isBefore(reactivateAt)
+                    )
                 }
             }.launchIn(viewModelScope)
+    }
+
+    private fun findReactivateDate(
+        interviewGroups: List<InterviewGroup>,
+        innerViewType: InnerViewType
+    ): LocalDate {
+        return if (interviewGroups.isEmpty()) {
+            LocalDate.now()
+        } else {
+            val lastDate = interviewGroups.first().createdAt
+                .withZoneSameInstant(ZoneId.systemDefault())
+                .toLocalDate()
+            when (innerViewType) {
+                InnerViewType.DAY -> lastDate.plusDays(1)
+                InnerViewType.WEEK -> lastDate.plusWeeks(1)
+                InnerViewType.MONTH -> lastDate.plusMonths(1)
+                InnerViewType.YEAR -> lastDate.plusYears(1)
+            }
+        }
     }
 
     fun addInnerViewGroup(innerViewId: String) {
