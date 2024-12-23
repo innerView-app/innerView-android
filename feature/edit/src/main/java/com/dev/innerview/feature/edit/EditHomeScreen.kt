4a88189538aae47eda7fe44aa91ef58e26dcbc59
p-chanmin <pc_m111@naver.com>
2024-12-23 21:23:00 +0900

    IV-45 refactor: GridItemSpan Spacer 리뷰 반영

--- feature/edit/src/main/java/com/dev/innerview/feature/edit/EditHomeScreen.kt
+++ feature/edit/src/main/java/com/dev/innerview/feature/edit/EditHomeScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
@@ -177,10 +178,8 @@ private fun InnerProjectList(
                     }
                 }
             }
-            repeat(interviewGroups.size % 2 + 1) {
-                item {
-                    Spacer(modifier = Modifier.height(80.dp))
-                }
+            item(span = { GridItemSpan(maxLineSpan) }) {
+                Spacer(modifier = Modifier.height(80.dp))
             }
         }
     }
