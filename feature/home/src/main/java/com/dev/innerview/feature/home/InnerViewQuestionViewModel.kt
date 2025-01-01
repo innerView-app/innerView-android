61532566826e863a2bfb00c3d086e50a5cf074d8
LeeHaiLim <hailim1997@gmail.com>
2025-01-01 23:39:47 +0900

    IV-20 feat: 질문 리스트 낙관적 업데이트

--- feature/home/src/main/java/com/dev/innerview/feature/home/InnerViewQuestionViewModel.kt
+++ feature/home/src/main/java/com/dev/innerview/feature/home/InnerViewQuestionViewModel.kt
@@ -11,8 +11,7 @@ import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -30,18 +29,19 @@ class InnerViewQuestionViewModel @Inject constructor(
     val innerViewQuestionUiState = _innerViewQuestionUiState.asStateFlow()
 
     fun fetchInnerViewQuestions(innerViewId: String) {
-        getInnerViewContentUseCase(innerViewId)
-            .onEach { innerViewContent ->
-                _innerViewQuestionUiState.update {
-                    it.copy(
-                        title = innerViewContent.innerView.title,
-                        interviewQuestions = innerViewContent.innerView.questions.toPersistentList()
-                    )
-                }
-            }.launchIn(viewModelScope)
+        viewModelScope.launch {
+            val innerViewContent = getInnerViewContentUseCase(innerViewId).first()
+            _innerViewQuestionUiState.update {
+                it.copy(
+                    title = innerViewContent.innerView.title,
+                    interviewQuestions = innerViewContent.innerView.questions.toPersistentList()
+                )
+            }
+        }
     }
 
     fun updateQuestions(innerViewId: String, oldIndex: Int, newIndex: Int) {
+        reorderQuestions(oldIndex, newIndex)
         viewModelScope.launch {
             runCatching {
                 reorderQuestionsUseCase(
@@ -52,6 +52,17 @@ class InnerViewQuestionViewModel @Inject constructor(
             }.onFailure {
                 _errorFlow.emit(IllegalArgumentException())
             }
+        }
+    }
+
+    private fun reorderQuestions(oldIndex: Int, newIndex: Int) {
+        val tmp = _innerViewQuestionUiState.value.interviewQuestions.toMutableList()
+
+        val item = tmp.removeAt(oldIndex)
+        tmp.add(newIndex, item)
+
+        _innerViewQuestionUiState.update {
+            it.copy(interviewQuestions = tmp.toPersistentList())
         }
     }
 }
