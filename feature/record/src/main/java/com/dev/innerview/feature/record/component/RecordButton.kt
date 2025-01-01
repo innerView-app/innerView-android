10997e81b5ce088dc8acda7af74b7fb74bf4b781
p-chanmin <pc_m111@naver.com>
2025-01-01 21:24:13 +0900

    IV-31 refactor: RecordButton indication 제거

--- feature/record/src/main/java/com/dev/innerview/feature/record/component/RecordButton.kt
+++ feature/record/src/main/java/com/dev/innerview/feature/record/component/RecordButton.kt
@@ -5,6 +5,7 @@ import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -69,7 +70,10 @@ fun RecordButton(
                 color = MaterialTheme.colorScheme.primary,
                 shape = CircleShape
             )
-            .clickable {
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null
+            ) {
                 when (recordState) {
                     RecordState.IDLE -> {
                         onRecordStart()
