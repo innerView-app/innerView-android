adf384f698763bb811c3c6553fa326a60697d3bc
LeeHaiLim <hailim1997@gmail.com>
2025-01-02 15:12:45 +0900

    IV-26 feat: 만료된 인터뷰 삭제

--- core/domain/src/main/java/com/dev/innerview/core/domain/usecase/GetInnerViewContentUseCase.kt
+++ core/domain/src/main/java/com/dev/innerview/core/domain/usecase/GetInnerViewContentUseCase.kt
@@ -3,11 +3,27 @@ package com.dev.innerview.core.domain.usecase
 import com.dev.innerview.core.data_api.InnerViewRepository
 import com.dev.innerview.core.model.InnerViewContent
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.onEach
+import java.time.ZonedDateTime
 import javax.inject.Inject
 
 class GetInnerViewContentUseCase @Inject constructor(
     private val innerViewRepository: InnerViewRepository
 ) {
-    operator fun invoke(innerViewId: String): Flow<InnerViewContent> =
-        innerViewRepository.getInnerViewContent(innerViewId)
+    operator fun invoke(innerViewId: String): Flow<InnerViewContent> {
+        return innerViewRepository
+            .getInnerViewContent(innerViewId)
+            .onEach { content ->
+                content.interviewGroups
+                    .filter {
+                        it.isRecording && it.createdAt.plusSeconds(5).isBefore(ZonedDateTime.now())
+                    }
+                    .forEach {
+                        innerViewRepository.deleteInterviewGroup(
+                            content.innerView.id,
+                            it.id
+                        )
+                    }
+            }
+    }
 }
