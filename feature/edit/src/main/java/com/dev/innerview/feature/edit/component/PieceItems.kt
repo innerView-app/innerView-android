82accbc9daedcb341140b9add9bfdb29326480f9
p-chanmin <pc_m111@naver.com>
2025-02-20 16:21:00 +0900

    IV-46 feat: EditScreen 영상 길이 조정 기능 추가

--- feature/edit/src/main/java/com/dev/innerview/feature/edit/component/PieceItems.kt
+++ feature/edit/src/main/java/com/dev/innerview/feature/edit/component/PieceItems.kt
@@ -3,22 +3,28 @@ package com.dev.innerview.feature.edit.component
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Circle
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -27,7 +33,7 @@ import com.dev.innerview.core.designsystem.component.getScaledVideoThumbnailByPa
 import com.dev.innerview.core.designsystem.theme.InnerViewTheme
 import com.dev.innerview.core.model.InterviewPiece
 import com.dev.innerview.core.model.Subtitle
-import com.dev.innerview.core.designsystem.R
+import com.dev.innerview.feature.edit.model.MediaItemSide
 import com.dev.innerview.feature.edit.model.MediaUiState
 
 @Composable
@@ -35,29 +41,101 @@ fun MediaItem(
     modifier: Modifier = Modifier,
     mediaUiState: MediaUiState,
     height: Dp = 50.dp,
+    updateMediaItemLengthen: (MediaItemSide, Int) -> Unit,
 ) {
     val thumbnail = getScaledVideoThumbnailByPath(mediaUiState.medium.filePath, height)
-    Surface(
+    Box(
         modifier = modifier
             .height(height)
-            .then(
-                if (mediaUiState.selected) {
-                    Modifier.border(
-                        width = 2.dp,
-                        color = MaterialTheme.colorScheme.surface,
-                        shape = RoundedCornerShape(8.dp)
-                    )
-                } else {
-                    Modifier
-                }
-            ),
-        color = MaterialTheme.colorScheme.tertiary,
-        shape = RoundedCornerShape(8.dp),
+            .clip(RoundedCornerShape(8.dp)),
+        contentAlignment = Alignment.Center
     ) {
-        RepeatImage(
-            image = thumbnail,
-            containerHeight = height
-        )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .clip(RoundedCornerShape(8.dp))
+                .then(
+                    if (mediaUiState.selected) {
+                        Modifier.border(
+                            width = 2.dp,
+                            color = MaterialTheme.colorScheme.surface,
+                            shape = RoundedCornerShape(8.dp)
+                        )
+                    } else {
+                        Modifier
+                    }
+                )
+        ) {
+            RepeatImage(
+                modifier = Modifier.fillMaxSize(),
+                image = thumbnail,
+                containerHeight = height
+            )
+        }
+        if (mediaUiState.selected) {
+            Column(
+                modifier = Modifier
+                    .width(6.dp)
+                    .height(height)
+                    .background(MaterialTheme.colorScheme.surface)
+                    .align(Alignment.CenterStart)
+                    .pointerInput(Unit) {
+                        detectDragGestures { change, dragAmount ->
+                            change.consume()
+                            updateMediaItemLengthen(MediaItemSide.START, dragAmount.x.toInt())
+                        }
+                    },
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(3.dp, Alignment.CenterVertically)
+            ) {
+                Icon(
+                    modifier = Modifier.size(4.dp),
+                    imageVector = Icons.Filled.Circle,
+                    contentDescription = null
+                )
+                Icon(
+                    modifier = Modifier.size(4.dp),
+                    imageVector = Icons.Filled.Circle,
+                    contentDescription = null
+                )
+                Icon(
+                    modifier = Modifier.size(4.dp),
+                    imageVector = Icons.Filled.Circle,
+                    contentDescription = null
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .width(6.dp)
+                    .height(height)
+                    .background(MaterialTheme.colorScheme.surface)
+                    .align(Alignment.CenterEnd)
+                    .pointerInput(Unit) {
+                        detectDragGestures { change, dragAmount ->
+                            change.consume()
+                            updateMediaItemLengthen(MediaItemSide.END, dragAmount.x.toInt())
+                        }
+                    },
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(3.dp, Alignment.CenterVertically)
+            ) {
+                Icon(
+                    modifier = Modifier.size(4.dp),
+                    imageVector = Icons.Filled.Circle,
+                    contentDescription = null
+                )
+                Icon(
+                    modifier = Modifier.size(4.dp),
+                    imageVector = Icons.Filled.Circle,
+                    contentDescription = null
+                )
+                Icon(
+                    modifier = Modifier.size(4.dp),
+                    imageVector = Icons.Filled.Circle,
+                    contentDescription = null
+                )
+            }
+        }
     }
 }
 
@@ -122,7 +200,8 @@ fun MediaItemPreview() {
                     duration = 0,
                 ),
                 selected = true,
-            )
+            ),
+            updateMediaItemLengthen = { _, _ -> },
         )
     }
 }
