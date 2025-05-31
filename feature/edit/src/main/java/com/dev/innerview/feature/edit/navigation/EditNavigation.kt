8adf5b9a9b6fa47d406d094fb287f458bf75904d
p-chanmin <pc_m111@naver.com>
2025-06-01 02:58:58 +0900

    IV-10 feat: edit screen deeplink 연결

--- feature/edit/src/main/java/com/dev/innerview/feature/edit/navigation/EditNavigation.kt
+++ feature/edit/src/main/java/com/dev/innerview/feature/edit/navigation/EditNavigation.kt
@@ -5,7 +5,9 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import androidx.navigation.navDeepLink
 import androidx.navigation.toRoute
+import com.dev.innerview.core.navigation.DEEP_LINK_BASE_PATH
 import com.dev.innerview.core.navigation.MainTabRoute
 import com.dev.innerview.core.navigation.Route
 import com.dev.innerview.feature.edit.EditHomeScreen
@@ -34,7 +36,9 @@ fun NavGraphBuilder.editNavGraph(
         )
     }
 
-    composable<Route.Edit> {
+    composable<Route.Edit>(
+        deepLinks = listOf(navDeepLink<Route.Edit>(basePath = "$DEEP_LINK_BASE_PATH/edit"))
+    ) {
         EditScreen(
             onShowErrorSnackBar = onShowErrorSnackBar,
             onBackClick = onBackClick
